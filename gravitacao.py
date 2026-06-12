be8dd8025b07a4d18851cd7d3b63c88e7b679339
Dedalos73 <matheusturossi2015@gmail.com>
2026-06-12 18:42:59 -0300

    Add files via upload

---
+++ gravitacao.py
@@ -0,0 +1,7 @@
+m1 = input('digite a massa do primeiro objeto em KG:')
+m2 = input('digite a massa do segundo objeto em KG:')
+d = input("digite a distancia entre os objeto em metros:")
+G = 6.674 *(10**-11)
+
+F = G * (float(m1)*float(m2))/(float(d)**2)
+print(f'A força gravitacional entre os corpos é de", {F},N')
